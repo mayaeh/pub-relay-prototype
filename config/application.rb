309332f04e4ed169bc0a5cde8cfb063ef9a11fe5
Maya Minatsuki <mayaeh@marimo-net.org>
2021-05-24 16:32:03 +0900

    Do not include autoload paths in the load path

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module PubRelay
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.add_autoload_paths_to_load_path = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
