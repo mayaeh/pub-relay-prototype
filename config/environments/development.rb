9235db47c6a59c9ee472cfa8b06bbae8b6c3a703
Maya Minatsuki <mayaeh@marimo-net.org>
2023-12-03 21:09:52 +0900

    add hostname config for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # see https://weseek.co.jp/tech/680/
+  config.hosts << "your-domain-here.example.com"
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
